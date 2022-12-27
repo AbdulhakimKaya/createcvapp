e29677ba8fe235213dcb9dd7ddb8696336dfe7df
KAYA-99 <abdulhakimkaya2199@gmail.com>
2022-12-27 16:58:55 +0300

    added templates

--- user_form/urls.py
+++ user_form/urls.py
@@ -1,6 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
+from user_form import views
 
+urlpatterns = [
+    path('', views.create_user_form, name="deneme_url"),
+    path('user_form/ajax/load-cities/', views.load_districts, name='ajax_load_cities'),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
