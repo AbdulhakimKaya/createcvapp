c2e19534de9b291d08cc67d3e03d564870cbaff3
KAYA-99 <abdulhakimkaya2199@gmail.com>
2022-12-27 16:59:08 +0300

    added admin

--- user_form/admin.py
+++ user_form/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from user_form.models import UserForm, City, District, Experience, WorkExperience, Education, Reference, Course
+
 # Register your models here.
+
+admin.site.register(UserForm)
+admin.site.register(City)
+admin.site.register(District)
+admin.site.register(Experience)
+admin.site.register(WorkExperience)
+admin.site.register(Education)
+admin.site.register(Reference)
+admin.site.register(Course)
