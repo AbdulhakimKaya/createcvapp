0746b99db1ddc09503ebf4d5752fde474d60ad5e
KAYA-99 <abdulhakimkaya2199@gmail.com>
2022-12-31 23:39:37 +0300

    edited urls

--- user_form/urls.py
+++ user_form/urls.py
@@ -10,7 +10,11 @@ urlpatterns = [
     path('example/', views.example, name="example"),
     path('about/', views.about, name="about"),
     path('user-form/', views.create_user_form, name="user-form"),
+    path('design-page/', views.design_page, name="design-page"),
+    path('pdf/<int:id>', views.users_render_pdf_view, name='user_pdf_view'),
     path('user_form/ajax/load-cities/', views.load_districts, name='ajax_load_cities'),
+    path('user_form/ajax/load-lang-levels/', views.load_lang_levels, name='ajax_load_lang_levels'),
+    path('user_form/ajax/load-ability-levels/', views.load_ability_levels, name='ajax_load_ability_levels'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
